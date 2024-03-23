1ee913ef6b949da4c7b8a2091a2a9bffc8f32283
Vlad <big_vlad2002@yahoo.com>
2024-03-23 20:46:32 +0200

    added weaponData objects for storing different weapons damage,firerate, etc

--- Proiect/MenuProject/Assets/Scripts/GunScript.cs
+++ Proiect/MenuProject/Assets/Scripts/GunScript.cs
@@ -3,34 +3,36 @@ using UnityEngine;
 
 public class GunScript : MonoBehaviour
 {
-    public float damage = 10f;
+    [SerializeField] GunData gunData;
+   /* public float damage = 10f;
     public float range = 100f;
-    public float firerate = 2f;
-    private float nextFireTime =0f;
+    public float firerate = 2f;*/
+    private float nextFireTime = 0f;
 
     public Camera playerCam;
 
-    public int magazineSize = 8;
+   /* public int magazineSize = 8;
     private int currentAmmo;
     public float reloadTime = 2f;
-    private bool currentlyReloading = false;
+    private bool currentlyReloading = false;*/
 
     public Animator animator;
 
     private void Start()
     {
-        currentAmmo = magazineSize;
+        gunData.currentAmmo = gunData.magazineSize;
+       // currentAmmo = magazineSize;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (currentlyReloading)
+        if (gunData.reloading)
         {
             return;
         }
 
-        if (currentAmmo <= 0 || Input.GetKeyDown(KeyCode.R))
+        if (gunData.currentAmmo <= 0 || Input.GetKeyDown(KeyCode.R))
         {
             StartCoroutine(ReloadWeapon());
             Debug.Log("Reloading....");
@@ -39,41 +41,41 @@ public class GunScript : MonoBehaviour
 
         if (Input.GetButton("Fire1") && Time.time >= nextFireTime)
         {
-            nextFireTime = Time.time + 1f/firerate;   
+            nextFireTime = Time.time + 1f / gunData.fireRate;
             Shoot();
         }
     }
 
     private void OnEnable()
     {
-        currentlyReloading = false ;
+        gunData.reloading= false;
         animator.SetBool("Reloading", false);
     }
 
     IEnumerator ReloadWeapon()
     {
-        currentlyReloading = true;
+        gunData.reloading = true;
         animator.SetBool("Reloading", true);
 
-        yield return new WaitForSeconds(reloadTime);
+        yield return new WaitForSeconds(gunData.reloadTime);
 
         animator.SetBool("Reloading", false);
-        currentAmmo = magazineSize;
-        currentlyReloading = false;
+        gunData.currentAmmo = gunData.magazineSize;
+        gunData.reloading = false;
     }
 
     void Shoot()
     {
 
         RaycastHit hit;
-        currentAmmo--;
-        if (Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hit, range))
+        gunData.currentAmmo--;
+        if (Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hit, gunData.range))
         {
-            Debug.Log(hit.transform.name);
+            Debug.Log(hit.transform.name + " " + gunData.damage);
             TargetScript targetScript = hit.transform.GetComponent<TargetScript>();
             if (targetScript != null)
             {
-                targetScript.TakeDamage(damage);
+                targetScript.TakeDamage(gunData.damage);
             }
         }
 
---
+++ Proiect/MenuProject/Assets/Scripts/ScriptableObjects/GunData.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Gun", menuName ="Weapon/Gun")]
+public class GunData : ScriptableObject
+{
+
+    public string weaponName;
+    public float damage;
+    public float range;
+
+    public int magazineSize;
+    public int currentAmmo;
+    public float fireRate;
+    public float reloadTime;
+
+    [HideInInspector]
+    public bool reloading;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
