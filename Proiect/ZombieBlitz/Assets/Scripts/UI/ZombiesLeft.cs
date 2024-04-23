f2f3ff0915e598caa96bf2c28fb1a31bdf5612e7
Vlad <big_vlad2002@yahoo.com>
2024-04-23 11:50:23 +0300

    more code documentation

--- Proiect/ZombieBlitz/Assets/Scripts/UI/ZombiesLeft.cs
+++ Proiect/ZombieBlitz/Assets/Scripts/UI/ZombiesLeft.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using TMPro;
 using UnityEngine;
-
+/// <summary>
+/// Display the number of zombies alive in the level
+/// </summary>
 public class ZombiesLeft : MonoBehaviour
 {
     public GameObject Allzombies;
@@ -27,34 +29,20 @@ public class ZombiesLeft : MonoBehaviour
 
     }
 
-   /* int CountZombies()      //maybe make this an IEnumerable and call it every .1 or .2 seconds?
+    IEnumerator CountZombies()     //count the zombies every .3 seconds
     {
         int count = 0;
-        ZombieController[] zombieControllers = Allzombies.GetComponentsInChildren<ZombieController>();
-        foreach(ZombieController zombieController in zombieControllers)
-        {
-            if (zombieController.GetHealth() > 0)
-            {
-                count++;
-            }
-        }
-        return count;
-    }*/
-
-    IEnumerator CountZombies()
-    {
-        int count = 0;
-        ZombieController[] zombieControllers = Allzombies.GetComponentsInChildren<ZombieController>();
+        ZombieController[] zombieControllers = Allzombies.GetComponentsInChildren<ZombieController>();  //get only objects which have a zombie controller script
         foreach (ZombieController zombieController in zombieControllers)
         {
-            if (zombieController.GetHealth() > 0)
-            {
-                count++;
+            if (zombieController.GetHealth() > 0)   //zombie game objects still exist a few seconds after reaching 0 health, to allow for the death animation to play.
+            {                       
+                count++;                            //thus we only count zombies who have more than 0 health as alive
             }
         }
         zombiesLeft = count;
         completedCount = true;
-        yield return new WaitForSeconds(.3f);
+        yield return new WaitForSeconds(.3f);       //will only count again after .3 seconds, as performing the count every frame would be redundant.
         completedCount = false;
     }
 }
